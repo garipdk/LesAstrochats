adaa9fe98cdfbfb59ae74e9e2c6d81731730e41f
Clément Fossez <clement.fossez@gmx.com>
2023-05-27 14:08:47 +0200

    Update

--- scenes/Mineral.gd
+++ scenes/Mineral.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@export var isBeingMined:bool = false
+@export var health:int = 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
