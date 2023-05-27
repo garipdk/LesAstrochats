208720052892d5259cb6fb712f3aa6bbd5074368
Clément Fossez <clement.fossez@gmx.com>
2023-05-27 15:55:38 +0200

    Update

--- scenes/HealthBar.gd
+++ scenes/HealthBar.gd
@@ -1,28 +1,22 @@
 extends Node2D
 
-@export var health:float = 100.0
+@export var health:float = 1;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	_updateState()
 	pass # Replace with function body.
 
 
-func giveDamage(damage:float):
-	health -= damage
-	$HealthBar.setHealthVal(health / 100.0)
-		
-	if health <= 0:
-		_destroy()
-		
+func setHealthVal(val):
+	health = clampf(val, 0, 1)
+	_updateState()
+	pass
 
+func _updateState():
+	get_node("Anchor").scale.x = health;
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	pass
-
-
-func _destroy():
-	queue_free()
 	pass
