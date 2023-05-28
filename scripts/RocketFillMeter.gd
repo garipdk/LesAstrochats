f0ede16a95e2d26b716019245a344f7881cb3e47
Clément Fossez <clement.fossez@gmx.com>
2023-05-28 11:35:42 +0200

    Update

--- scripts/RocketFillMeter.gd
+++ scripts/RocketFillMeter.gd
@@ -1,19 +1,22 @@
 extends Node
 
-
-var _fillValue:int = 0
 var _fillMax:int = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_updateState()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	_updateState()
 	pass
 
 func _updateState():
-	$ProgressBar.value = float(_fillValue) / float(_fillMax) * 100
-	$ProgressBar/RichTextLabel.text = str(_fillValue) + "/" + str(_fillMax)
+	var scoreVal = clamp(Global.score, 0, _fillMax)
+	$ProgressBar.value = float(scoreVal) / float(_fillMax) * 100
+	$ProgressBar/RichTextLabel.text = str(scoreVal) + "/" + str(_fillMax)
 	pass
+
+
